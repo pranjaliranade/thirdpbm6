c7c32bf02959d2981fe65c03b064a92f5992f728
unknown <ranadepranjali@gmail.com>
2024-11-17 17:28:03 +0530

    change1

--- src/ImageDisplay.js
+++ src/ImageDisplay.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './ImageDisplay.css'; // Create this CSS file for any styling you might want
-//import images001 from './media/images001.jpg';
+import images001 from './media/images001.jpg';
 
 const ImageDisplay = () => {
-  const [imageNumber, setImageNumber] = useState('');
-  const [imageSrc, setImageSrc] = useState('');
+  const [imageNumber, setImageNumber] = useState('001');
+  const [imageSrc, setImageSrc] = useState('./media/images001.jpg');
 
   const handleChange = (e) => {
     setImageNumber(e.target.value);
@@ -13,7 +13,7 @@ const ImageDisplay = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (imageNumber !== '') {
-      setImageSrc(`./media/image001.jpg`); // Adjust this path as necessary 
+      setImageSrc(images001); // Adjust this path as necessary 
     }
   };
 
@@ -26,9 +26,9 @@ const ImageDisplay = () => {
         </label>
         <button type="submit">Show Image</button>
       </form>
-      <img src={`./media/images${imageNumber}.jpg`}  />
+      <img src={require(`./media/images${imageNumber}.jpg`).default} alt={`./media/images${imageNumber}`} />
       
-        {/* {imageSrc && } alt={`./media/${imageNumber}`}*/}
+        {/* {imageSrc && } alt={`../media/${imageNumber}`} require(`./media/images${imageNumber}.jpg`).default*/}
     </div> 
   );
 };

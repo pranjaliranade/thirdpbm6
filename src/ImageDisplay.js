3830376db3c4187f03ac05c97c7bd253d03c052f
unknown <ranadepranjali@gmail.com>
2024-11-17 19:17:34 +0530

    nextcommit

--- src/ImageDisplay.js
+++ src/ImageDisplay.js
@@ -1,19 +1,15 @@
-import React, { useState, useRef } from 'react';
-import './ImageDisplay.css'; // Create this CSS file for any styling you might want
-import images001 from './media/images001.jpg';
+import React, { useRef, useState } from 'react';
+import './ImageDisplay.css'; // Ensure you have this file for styles
 
 const ImageDisplay = () => {
-  const [imageNumber, setImageNumber] = useState('001');
-  const [imageSrc, setImageSrc] = useState('./media/images001.jpg');
-
-  const handleChange = (e) => {
-    setImageNumber(e.target.value);
-  };
+  const imageNumberRef = useRef();
+  const [imageSrc, setImageSrc] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const imageNumber = imageNumberRef.current.value;
     if (imageNumber !== '') {
-      setImageSrc(images001); // Adjust this path as necessary 
+      setImageSrc(`./media/images${imageNumber}.jpg`); // Adjust the path according to your directory structure
     }
   };
 
@@ -22,15 +18,16 @@ const ImageDisplay = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Enter Image Number:
-          <input type="number" value={imageNumber} onChange={handleChange} />
+          <input type="number" ref={imageNumberRef} />
         </label>
         <button type="submit">Show Image</button>
       </form>
-      <img src={require(`./media/images${imageNumber}.jpg`).default} alt={`./media/images${imageNumber}`} />
-      
-        {/* {imageSrc && } alt={`../media/${imageNumber}`} require(`./media/images${imageNumber}.jpg`).default*/}
-    </div> 
+      {imageSrc && <img src={require(`./media/images${imageNumberRef.current.value}.jpg`).default} 
+      alt={`Image ${imageNumberRef.current.value}`} />}
+    </div>
   );
 };
 
 export default ImageDisplay;
+
+/* {imageSrc && } alt={`../media/${imageNumber}`} require(`./media/images${imageNumber}.jpg`).default*/
